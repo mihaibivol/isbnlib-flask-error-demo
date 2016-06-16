7817b5a4c0657fc334e9b5f84b2851a198520b62
Mihai Bivol <mihai.bivol@cern.ch>
2016-06-16 18:08:06 +0200

    Retry 5 times

--- run-test.sh
+++ run-test.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+set -e
 
-./app.py &> /tmp/flasklog.log &
-sleep 1
-phantomjs test.js
+rm -f /tmp/flasklog.log
 
-kill $(ps aux | egrep '.*python.*app.py$' | awk '{ print $2 }')
+function test_case {
+  ./app.py &>> /tmp/flasklog.log &
+  sleep 1
+  phantomjs test.js
+  
+  kill $(ps aux | egrep '.*python.*app.py$' | awk '{ print $2 }')
+  
+}
+
+for i in $(seq 1 5); do
+  echo "Run number ${i}"
+  test_case
+done
 
 cat /tmp/flasklog.log
 grep "Errno 11" /tmp/flasklog.log
-
