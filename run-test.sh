aa2e9c3f3082aa6bee5f7a365b9f44c5c8e8af40
Mihai Bivol <mihai.bivol@cern.ch>
2016-06-16 17:44:47 +0200

    sleep more, no requests on Travis

--- run-test.sh
+++ run-test.sh
@@ -3,7 +3,7 @@
 ./app.py &> /tmp/flasklog.log &
 sleep 3
 links2 -g http://localhost:5000 &> /dev/null &
-sleep 1
+sleep 3
 kill $!
 
 kill $(ps aux | egrep '.*python.*app.py$' | awk '{ print $2 }')
