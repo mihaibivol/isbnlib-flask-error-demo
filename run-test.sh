f68a03132dfcab3856e86c11871f12a17ed90380
Mihai Bivol <mihai.bivol@cern.ch>
2016-06-16 17:25:41 +0200

    Add forgotten test to trigger build

---
+++ run-test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+./app.py &> /tmp/flasklog.log &
+sleep 3
+links2 -g http://localhost:5000 &> /dev/null &
+sleep 1
+kill $!
+
+kill $(ps aux | egrep '.*python.*app.py$' | awk '{ print $2 }')
+
+cat /tmp/flasklog.log
+grep Errno 11 /tmp/flasklog.log
+
